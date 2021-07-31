f51ac8a21f12962dc24ecc3382e64dc975ca20cf
unknown <rajeshroyal896@gmail.com>
2021-07-31 09:17:19 +0530

    login page, react-feather, project-theme - correction

--- src/Config/ProjectTheme.js
+++ src/Config/ProjectTheme.js
@@ -1,6 +1,6 @@
 export const projectTheme = {
-  background: "bg-orange-300",
-  borderColor: "border-orange-400",
-  textColor: "text-orange-400",
-  closeXButtonColor: "text-orange-400",
+  background: "bg-pink-300",
+  borderColor: "border-pink-400",
+  textColor: "text-pink-400",
+  closeXButtonColor: "text-pink-400",
 };
