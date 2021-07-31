5fe54193393422fb19a65d019315c52dea5c1e62
unknown <rajeshroyal896@gmail.com>
2021-07-31 14:53:32 +0530

    protected route configuration with server

--- client/src/Components/common/ProtectedRoute.js
+++ client/src/Components/common/ProtectedRoute.js
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import React, { useEffect, useState } from "react";
 import { Redirect, Route } from "react-router-dom";
+import { IS_USER_LOGGED_IN } from "../../core/gql-operations/mutation/is-user-logged-in";
 
 export const ProtectedRoute = ({ component: Component, ...rest }) => {
-  const [userLoggedIn, setUserLoggedIn] = useState(true);
-  const [loading, setLoading] = useState(false);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [isUserLoggedIn] = useMutation(IS_USER_LOGGED_IN);
+
+  //   run effect on first load
+  useEffect(() => {
+    isUserLoggedIn({
+      variables: {
+        userId: "xyz",
+      },
+    })
+      .then((data) => {
+        setUserLoggedIn(true);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setUserLoggedIn(false);
+        setLoading(true);
+      });
+  }, [isUserLoggedIn]);
 
   return !loading ? (
     <Route
